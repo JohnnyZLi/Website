018303690065fd70df345d98bf7dbf20bf2a8e7c
Johnny <92900710+JohnnyZLi@users.noreply.github.com>
2026-08-06 01:07:46 -0700

    Expose active report progress state

--- technical-report.js
+++ technical-report.js
@@ -83,6 +83,7 @@ if (toc instanceof HTMLElement && progress instanceof HTMLElement) {
             ${transitionOverride}
           }
         `);
+        if (activeIndex >= 0) progressFill.style.setProperty('--report-progress', progressValue);
         return;
       }
 
